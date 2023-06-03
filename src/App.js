8a736a028c6d2aaf2df53c6b238fcc6b218a3614
Himanshu Gupta <hg1293@gmail.com>
2023-06-03 12:07:42 +0530

    3

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Form from "./component/forms";
+
+function App() {
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
+
+export default App;
